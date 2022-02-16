6e2bd51b5ef0dfafc46a0dfc19ff509bf1f739a9
Edilson Matola <edilsonmatola13@gmail.com>
2022-02-16 22:18:24 +0200

    chore: Added message

--- crud basics/script/script.js
+++ crud basics/script/script.js
@@ -1,1 +1,21 @@
 let form = document.getElementById("form");
+
+let input = document.getElementById("inputArea");
+
+let message = document.getElementById("msg");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Button clicked!");
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+    message.innerText = "Post cannot be empty.";
+    console.log("Failure");
+  } else {
+    message.innerText = " ";
+    console.log("Success");
+  }
+};
